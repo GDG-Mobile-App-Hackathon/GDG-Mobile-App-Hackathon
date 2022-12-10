b5e15f1363790ef9f675d26c54e6e4aa43374545
Cyb3r7ron <kiduskyle@gmail.com>
2022-12-10 11:48:20 -0500

    splashhhh

--- tele_birr/lib/splash_screen.dart
+++ tele_birr/lib/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tele_birr/pages/home.dart';
+import 'package:tele_birr/pages/homepage.dart';
 //import 'package:tele_birr/pages/homepage.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -18,6 +19,25 @@ class _SplashScreenState extends State<SplashScreen> {
       decoration: const BoxDecoration(
         image: DecorationImage(
             image: AssetImage("asset/image/BGPic.png"), fit: BoxFit.cover),
+      ),
+      child: GestureDetector(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.greenAccent[400],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: const Center(
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
